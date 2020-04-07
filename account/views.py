80ebfb474400137c236db1d98733b199de3fc9a1
lwairore <kwairore@gmail.com>
2020-04-08 00:21:52 +0300

    - Decorate dashboard view with the `login_required` decorator of the authentication framework.

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from . import forms
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_login(request):
@@ -24,3 +25,9 @@ def user_login(request):
     else:
         form = forms.LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
