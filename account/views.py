0ef1e79e665e56527a15df8af6e6e6c8a3da17ff
lwairore <kwairore@gmail.com>
2020-04-07 23:48:06 +0300

    - Create a login view.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from . import forms
 
 # Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=['password']) # hecks user credentials and returns a User object if they are right
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # sets the user in the current session.
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = forms.LoginForm()
+    return render(request, 'account/login.html', {'form': form})
