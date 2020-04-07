84df80dcb1cce7e40c0e3d714eba0ee499a3a1c6
lwairore <kwairore@gmail.com>
2020-04-08 00:00:05 +0300

    - Configure for psql database

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'PORT': '',
+        'NAME': 'social-bookmark',
+        'USER': 'karangu',
+        'PASSWORD': 'lmzongolo8754',
+        'HOST': '127.0.0.1'
     }
 }
+
 
 
 # Password validation
