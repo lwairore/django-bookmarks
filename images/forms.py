c87eba2d9617d422ff29739578afe3daf850d937
lwairore <kwairore@gmail.com>
2020-04-09 00:58:03 +0300

    - Define clean_url() method to clean the url field.

--- images/forms.py
+++ images/forms.py
@@ -11,3 +11,11 @@ class ImageCreateForm(forms.ModelForm):
         """
             Users will not enter the image URL directly in the form. Instead, we will provide them with a JavaScript tool to choose an image from an external site, and our form will receive its URL as a parameter. We override the default widget of the url field to use a HiddenInput widget. This widget is rendered as an HTML input element with a type="hidden" attribute. We use this widget because we don't want this field to be visible to users.
         """
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions')
+        return url
