78496dd9c0cdc30f09d3bff440cfcd9a5898ab86
lwairore <kwairore@gmail.com>
2020-04-08 14:59:27 +0300

    ...

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -140,4 +140,4 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBacked'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
