0176e597511d36b57570437f08c048bdd76bfec2
lwairore <kwairore@gmail.com>
2020-04-09 00:55:12 +0300

    - Add a docstring

--- images/forms.py
+++ images/forms.py
@@ -8,3 +8,6 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput
         }
+        """
+            Users will not enter the image URL directly in the form. Instead, we will provide them with a JavaScript tool to choose an image from an external site, and our form will receive its URL as a parameter. We override the default widget of the url field to use a HiddenInput widget. This widget is rendered as an HTML input element with a type="hidden" attribute. We use this widget because we don't want this field to be visible to users.
+        """
