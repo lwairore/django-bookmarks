148a1d6c8928c2cabf228ac687f2fef9295e8432
lwairore <kwairore@gmail.com>
2020-04-08 22:12:21 +0300

    - Create model Profile.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Profile(models.Model):
+    """
+        In order to keep your code generic, use the `get_user_model()` method to retrieve the user model and the `AUTH_USER_MODEL` setting to refer to it when defining a model's relations to the user model, instead of referring to the auth user model directly.
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
