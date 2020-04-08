21f4e43ad0c3ff3f2cd50b3c60fcfffaa0ebf4ae
lwairore <kwairore@gmail.com>
2020-04-08 23:45:28 +0300

    - Add a custom email authentication backend.

---
+++ account/authentication.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+
+class EmailAuthBackend(object):
+    """
+        Authenticate using an e-mail address.
+    """
+
+    def authenticate(self, request, email=None, password=None):
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+
+        def get_user(self, user_id):
+            try:
+                return User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                return None
