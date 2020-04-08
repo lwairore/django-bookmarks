5d174d9cedcf2f39c9346617116aaee427aa69d4
lwairore <kwairore@gmail.com>
2020-04-09 00:51:50 +0300

    - Register the Image model into the admin site.

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
