150f41b68f78af34540109a95164cd3557285ceb
lwairore <kwairore@gmail.com>
2020-04-09 07:19:05 +0300

    - Configure to connect with ngrok.

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,6 +3,6 @@
         myBookmarklet();
     }
     else {
-        document.body.appendChild(document.createElement('script')).src='https://96801734.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+        document.body.appendChild(document.createElement('script')).src='https://7c74171c.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
 })();
--- static/js/bookmarklet.js
+++ static/js/bookmarklet.js
@@ -1,6 +1,6 @@
 (function(){
     var jquery_version = '3.3.1';
-    var site_url = 'https://96801734.ngrok.io/';
+    var site_url = 'https://7c74171c.ngrok.io/';
     var static_url = site_url + 'static/';
     var min_width = 100;
     var min_height = 100;
