e191aa3121b200c83c3eba558a2d1a2a2594a62a
lwairore <kwairore@gmail.com>
2020-04-09 08:47:07 +0300

    - Add field following to user.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -20,8 +21,20 @@ class Contact(models.Model):
     user_to = models.ForeignKey('auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
+    """
+        In the preceding example, we tell Django to use our custom intermediary model for the relationship by adding through=Contact to the ManyToManyField. This is a many-to-many relationship from the User model to itself: we refer to 'self' in the ManyToManyField field to create a relationship to the same model.
+    """
+
     class Meta:
         ordering = ('-created',)
 
     def __str__(self):
         return '{} follows {}'.format(self.user_from, self.user_to)
+
+# Add the following field to User dynamically
+# Add following field to User dynamically
+User.add_to_class('following',
+                  models.ManyToManyField('self',
+                                         through=Contact,
+                                         related_name='followers',
+                                         symmetrical=False))
