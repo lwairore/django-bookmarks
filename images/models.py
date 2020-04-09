9b51bd1e87de7a34041d3736d3194e5334dc3bb7
lwairore <kwairore@gmail.com>
2020-04-09 20:50:15 +0300

    - Add field total_likes to image.

--- images/models.py
+++ images/models.py
@@ -4,19 +4,26 @@ from django.utils.text import slugify
 from django.urls import reverse
 
 # Create your models here.
+
+
 class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slug= models.SlugField(max_length=200, blank=True)
+    slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
-    created = models.DateField(auto_now_add=True, db_index=True) # We use `bd_index=True` so that Django creates an index in the database for this field.
-    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+    # We use `bd_index=True` so that Django creates an index in the database for this field.
+    created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
+
     """
         Database indexes improve query performance. Consider setting db_index=True for fields that you frequently query using filter(), exclude(), or order_by(). ForeignKey fields or fields with unique=True imply the creation of an index. You can also use Meta.index_together to create indexes for multiple fields.
     """
-    
+
     def __str__(self):
         return self.title
 
@@ -30,5 +37,3 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse('images:detail', args=[self.id, self.slug])
-        
-
