a34d6c8ef2aa0db386bcc60f546fbf480597dd2a
lwairore <kwairore@gmail.com>
2020-04-09 20:16:17 +0300

    - Fix 'QuerySet' object has no attribute 'selected_related'

--- account/views.py
+++ account/views.py
@@ -86,13 +86,13 @@ def dashboard(request):
     if following_ids:
         # If user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions.selected_related('user', 'user__profile')\
-        .prefetch_related('target')[:10]
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
 
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard', 'actions': actions})
-
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 def register(request):
     if request.method == 'POST':
