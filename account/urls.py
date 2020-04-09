5defd91dc863b1f3598ada0aa529831ecd474337
lwairore <kwairore@gmail.com>
2020-04-09 14:40:40 +0300

    - Add URL pattern for user_follow.

--- account/urls.py
+++ account/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # change password urls
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 
     path('register/', views.register, name='register'),
 
@@ -20,20 +22,21 @@ urlpatterns = [
     #     name='password_reset'),
     path('', include('django.contrib.auth.urls')),
     path('password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done'),
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'),
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
     path('reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete'),
-    
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+
     path('edit/', views.edit, name='edit'),
 
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
-    
-        
+    path('users/follow/', views.user_follow, name='user_follow'),
+
+
     path('', views.dashboard, name='dashboard'),
 ]
