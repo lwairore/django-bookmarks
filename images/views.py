164e2f2445c4dfefdba07f62b55ac313b729e270
lwairore <kwairore@gmail.com>
2020-04-09 07:34:54 +0300

    - Create image_like view.

--- images/views.py
+++ images/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from . import forms
 from . import models
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 @login_required
@@ -37,3 +39,20 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = models.Image.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'OK'})
+        except:
+            pass
+    return JsonResponse({'status': 'OK'})
