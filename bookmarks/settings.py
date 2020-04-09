dff3fe1ab8143b2b2956120b1216ff0c9c778be9
lwairore <kwairore@gmail.com>
2020-04-09 07:06:54 +0300

    - Add javascript to allow bookmarklet.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dz==8zw6l$_ixbj!2pfrz0c7z97tf29jsccw5%zegt^x(fkd@k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', '96801734.ngrok.io']
 
 
 # Application definition
